ce147fd09e7c550a5a8d2803701fd363c84f4abe
JasonLee0905 <101697154+JasonLee0905@users.noreply.github.com>
2022-05-31 04:39:05 +0800

    HTML_codes
    這是用HTML寫我的個人樹梅派軟體安裝網站語法

---
+++ maincode.cs
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>C110196103</title>
+  </head>
+  <body style="background-color:#C9FFFF;">
+
+<div class="jumbotron bg-info text-white" > 
+  <h1> Welcome to Jason's website! </h1>
+</div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Jason Lee</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://ccet.nkust.edu.tw">CCET website</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            My mid-term project (what I downloaded)
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item"href="/wordpress">Wordpress</a></li>
+            <li><a class="dropdown-item"href="/update.php">Drupal</a></li>
+                            <li><a class="dropdown-item"href="/phpmyadmin/">Phpmyadmin</a></li>
+                            <li><a class="dropdown-item"href="/owncloud/">Owncloud</a></li>
+                            <li><a class="dropdown-item"href="/contao-3.5.40/contao/main.php">Contao</a></li>
+             <li><hr class="dropdown-divider"></li>
+             <li><a class="dropdown-item" href="#">Youtube Links</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<table class="table table-dark table-striped">
+ <tr><td>System name</td><td>Website name</td><td>What is it?</td><td>
+ <tr><td>Wordpress</td><td>/wordpress/</td><td>WordPress is the simplest, most popular way to create your own website or blog</td><td>
+ <tr><td>Drupal</td><td>/update.php</td><td>Drupal is a free, open-source content management system (CMS) with a large, supportive community</td><td>
+ <tr><td>Phpmyadmin</td><td>/phpmyadmin/</td><td>PhpMyAdmin is an open-source software tool, which is written in PHP</td><td>
+ <tr><td>Owncloud</td><td>/owncloud/</td><td>OwnCloud is a suite of client server software for creating and using file hosting services.</td><td>
+ <tr><td>Contao</td><td>/contao-3.5.40/contao/main.php</td><td>Contao is a powerful open source CMS that allows you to create professional websites and scalable web applications.</td><td>
+
+</table>
+
+         </div>
+
+
+<hr>
+<div class="container">
+</div>
+
+
+<div class="container row text-left">
+    <div class="row">
+      <div class="span4"></div>
+      <div class="span4"><img class="center-block" src="https://www.gbcloud.net/blog/wp-content/uploads/2021/11/Cloudd.jpg" width = "800" /></div>
+      <div class="span4"></div>
+    </div>
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
